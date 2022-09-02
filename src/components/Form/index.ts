ac672db1100653af3c8427e5505c6b5e441b36ef
Lukita <lucmaieski@gmail.com>
2022-09-02 14:07:07 -0300

    Add form initial css

--- src/components/Form/index.ts
+++ src/components/Form/index.ts
@@ -1,13 +1,31 @@
 import styled from 'styled-components';
 
 export const FormContainer = styled.div`
+    display: flex;
+    margin-left: 2em;
+    margin-right: 2em;
+    margin-top: 2em;
+    align-items: center;
 
+    input[type=text]:focus {
+        outline: none !important;
+        border: 2px solid #00db87;
+    }
 `;
 
 export const FormInput = styled.input`
-
+    border: 2px solid black;
+    border-radius: 6px;
+    height: 3.4em;
+    width: 90%;
+    padding-left: 1em;
 `;
 
 export const FormButton = styled.button`
+    flex-direction: column;
+    background: none;
+    border: none;
+    margin-left: 1em;
 
+    font-size: 1.4em;
 `;
