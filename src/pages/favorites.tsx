d8c754eada3f341c5b6dbda07e1a3492267f8cdd
Lukita <lucmaieski@gmail.com>
2022-09-03 17:58:27 -0300

    Finish favorite page

--- src/pages/favorites.tsx
+++ src/pages/favorites.tsx
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Footer from '../components/Footer/Footer';
 import Header from '../components/Header/Header';
 import Form from '../components/Form/Form';
 import { CardHomeContainer } from '../components/Card';
 import GlobalContainer from '../styles/Index';
+import Card from '../components/Card/Card';
 
 function Favorites() {
+  const [storagedArticles, setStoragedArticles] = useState([]);
+
+  useEffect(() => {
+    const currentStorage = JSON.parse(localStorage.getItem('favoritesArticles'));
+    setStoragedArticles(currentStorage);
+  }, []);
+
   return (
     <GlobalContainer>
       <Header />
       <Form />
-      <CardHomeContainer></CardHomeContainer>
+      <CardHomeContainer>
+        {storagedArticles.map((article) => <Card article={article} />)}
+      </CardHomeContainer>
       <Footer />
     </GlobalContainer>
   );
